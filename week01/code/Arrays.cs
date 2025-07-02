f862fa8705d515e507ed74e30882c4e2264a60cd
Sembatya2020 <ivansmbt@gmail.com>
2025-07-02 20:28:51 +0300

    week01

--- week01/code/Arrays.cs
+++ week01/code/Arrays.cs
@@ -13,7 +13,13 @@ public static class Arrays
         // step by step before you write the code. The plan should be clear enough that it could
         // be implemented by another person.
 
-        return []; // replace this return statement with your own
+        double[] result = new double[length];
+        for (int i = 0; i < length; i++)
+        {
+            result[i] = number * (i + 1);
+        }
+
+        return result; // replace this return statement with your own
     }
 
     /// <summary>
@@ -29,5 +35,10 @@ public static class Arrays
         // Remember: Using comments in your program, write down your process for solving this problem
         // step by step before you write the code. The plan should be clear enough that it could
         // be implemented by another person.
+
+        int n = data.Count;
+        List<int> LastPart = data.GetRange(n - amount, amount);
+        data.RemoveRange(n - amount, amount);
+        data.InsertRange(0, LastPart);
     }
 }
